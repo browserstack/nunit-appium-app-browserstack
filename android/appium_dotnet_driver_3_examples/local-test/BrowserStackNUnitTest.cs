09141759792f85e3d628142a2071e78baf58d2ce
Gaurav Shah <gaurav.s@browserstack.com>
2020-09-23 19:17:22 +0530

    android appium dotnet driver 3 and 4 examples

---
+++ android/appium_dotnet_driver_3_examples/local-test/BrowserStackNUnitTest.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using OpenQA.Selenium.Appium.Android;
+using BrowserStack;
+using System.Runtime.InteropServices;
+
+namespace android.local
+{
+	public class BrowserStackNUnitTest
+	{
+		protected AndroidDriver<AndroidElement> driver;
+		protected string profile;
+		protected string device;
+		private Local browserStackLocal;
+
+		public BrowserStackNUnitTest(string profile, string device)
+		{
+				this.profile = profile;
+				this.device = device;
+		}
+
+		[SetUp]
+		public void Init()
+		{
+			NameValueCollection caps = ConfigurationManager.GetSection("capabilities/" + profile) as NameValueCollection;
+			NameValueCollection devices = ConfigurationManager.GetSection("environments/" + device) as NameValueCollection;
+
+			DesiredCapabilities capability = new DesiredCapabilities();
+
+			foreach (string key in caps.AllKeys)
+			{  
+				capability.SetCapability(key, caps[key]);
+			}
+
+			foreach (string key in devices.AllKeys)
+			{
+				capability.SetCapability(key, devices[key]);
+			}
+
+			String username = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
+			if (username == null)
+			{
+				username = ConfigurationManager.AppSettings.Get("user");
+			}
+
+			String accesskey = Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY");
+			if (accesskey == null)
+			{
+				accesskey = ConfigurationManager.AppSettings.Get("key");
+			}
+
+			capability.SetCapability("browserstack.user", username);
+			capability.SetCapability("browserstack.key", accesskey);
+
+			String appId = Environment.GetEnvironmentVariable("BROWSERSTACK_APP_ID");
+			if (appId != null)
+			{
+				capability.SetCapability("app", appId);
+			}
+
+			// if the platform is Windows, enable local testing fropm within the test
+			// for Mac and GNU/Linux, run the local binary manually to enable local testing (see the docs)
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+					&& capability.GetCapability("browserstack.local") != null
+					&& capability.GetCapability("browserstack.local").ToString() == "true")
+			{
+				browserStackLocal = new Local();
+				List<KeyValuePair<string, string>> bsLocalArgs = new List<KeyValuePair<string, string>>() {
+						new KeyValuePair<string, string>("key", accesskey)
+				};
+				bsLocalArgs.Add(new KeyValuePair<string, string>("logfile", "~/.browserstack/logs.txt"));
+				browserStackLocal.start(bsLocalArgs);
+			}
+
+			driver = new AndroidDriver<AndroidElement>(new Uri("http://" + ConfigurationManager.AppSettings.Get("server") + "/wd/hub/"), capability);
+		}
+
+		[TearDown]
+		public void Cleanup()
+		{
+			driver.Quit();
+			if (browserStackLocal != null)
+			{
+				browserStackLocal.stop();
+			}
+		}
+
+	}
+}
---
+++ android/appium_dotnet_driver_4_examples/first-test/FirstTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Support.UI;
+
+namespace android.first
+{
+    [TestFixture("first","pixel-3")]
+    public class SingleTest : BrowserStackNUnitTest
+    {
+        public SingleTest(string profile, string device) : base(profile,device){}
+
+        [Test]
+        public void searchWikipedia()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            AndroidElement searchElement = (AndroidElement)wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia")));
+            searchElement.Click();
+
+            AndroidElement insertTextElement = (AndroidElement)wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("org.wikipedia.alpha:id/search_src_text")));
+            insertTextElement.SendKeys("BrowserStack");
+            Thread.Sleep(5000);
+
+            ReadOnlyCollection<AndroidElement> allProductsName = driver.FindElements(By.ClassName("android.widget.TextView"));
+            Assert.True(allProductsName.Count > 0);
+        }
+    }
+}
