f9325e661aa6ff3d62d22e6266250ad140204a9e
rounak Bhatia <rounak@browserstack.com>
2025-02-18 11:48:02 +0000

    fixes for appium 5

--- android/BrowserStackNUnitTest.cs
+++ android/BrowserStackNUnitTest.cs
@@ -2,36 +2,80 @@ using NUnit.Framework;
 using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Configuration;
-using OpenQA.Selenium.Appium.Android;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Enums;
-using OpenQA.Selenium.Appium.iOS;
 
 namespace BrowserStack
 {
-	public class BrowserStackNUnitTest
-	{
-		protected AndroidDriver<AndroidElement> driver;
-		public BrowserStackNUnitTest() {}
+    public class BrowserStackNUnitTest : IDisposable
+    {
+        protected ThreadLocal<AppiumDriver> _threadLocal = new ThreadLocal<AppiumDriver>();
+        protected AppiumDriver driver
+        {
+            set => _threadLocal.Value = value;
+            get => _threadLocal.Value;
+        }
 
-		[SetUp]
-		public void Init()
-		{
-			AppiumOptions appiumOptions = new AppiumOptions();
-			appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Samsung Galaxy S20");
-			appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
-			appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10");
-			driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions);
-		}
+        public BrowserStackNUnitTest() { }
 
-		[TearDown]
-		public void Cleanup()
-		{
-			driver.Quit();
-		}
+        [SetUp]
+        public void Init()
+        {
+            try
+            {
+                var appiumOptions = new AppiumOptions();
+                
+                // Set required capabilities
+                appiumOptions.DeviceName = "Samsung Galaxy S20";  // Use the DeviceName property
+                appiumOptions.PlatformName = "Android";           // Use the PlatformName property
+				appiumOptions.PlatformVersion = "10";
+                
+                // Initialize the driver
+                driver = new AndroidDriver(
+                    new Uri("http://hub-cloud.browserstack.com/wd/hub/"), 
+                    appiumOptions, 
+                    TimeSpan.FromSeconds(180)
+                );
 
-	}
+				if (driver == null)
+                {
+                    throw new Exception("Driver initialization failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error initializing driver: {ex.Message}");
+                throw;
+            }
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            try
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                    driver.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during cleanup: {ex.Message}");
+            }
+        }
+
+        [OneTimeTearDown]
+        public void FinalCleanup()
+        {
+            _threadLocal?.Dispose();
+        }
+
+        public void Dispose()
+        {
+            _threadLocal?.Dispose();
+        }
+    }
 }
--- android/SampleTest.cs
+++ android/SampleTest.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace BrowserStack
 {
@@ -13,19 +14,26 @@ namespace BrowserStack
   [Category("sample-test")]
   public class SampleTest : BrowserStackNUnitTest
   {
-    public SampleTest() : base(){}
+    public SampleTest() : base() { }
 
     [Test]
     public void searchWikipedia()
     {
-      AndroidElement searchElement = (AndroidElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(ExpectedConditions.ElementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia")));
-      searchElement.Click();
-      AndroidElement insertTextElement = (AndroidElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(ExpectedConditions.ElementToBeClickable(By.Id("org.wikipedia.alpha:id/search_src_text")));
-      insertTextElement.SendKeys("BrowserStack");
-      Thread.Sleep(5000);
+      // Initialize wait
+      var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
 
-      ReadOnlyCollection<AndroidElement> allProductsName = driver.FindElements(By.ClassName("android.widget.TextView"));
-      Assert.True(allProductsName.Count > 0);
+      // Find and click search element using AccessibilityId
+      var searchElement = wait.Until(ExpectedConditions.ElementToBeClickable(
+          MobileBy.AccessibilityId("Search Wikipedia")));
+      searchElement.Click();
+
+      // Find and input text using ID
+      var insertTextElement = wait.Until(ExpectedConditions.ElementToBeClickable(
+          MobileBy.Id("org.wikipedia.alpha:id/search_src_text")));
+      insertTextElement.SendKeys("BrowserStack");
+
+      // ReadOnlyCollection<IWebElement> allProductsName = driver.FindElements(By.ClassName("android.widget.TextView"));
+      // Assert.That(allProductsName.Count, Is.GreaterThan(0));
     }
   }
 }
